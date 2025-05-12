c617f51723a6d7f45c8deb8b3dfaec369e57165d
Waseemko-ayman <wasimabdelhadi78@gmail.com>
2025-05-12 15:52:49 +0300

    Add Save Button With Logic

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let counter = document.getElementById("counter");
 let addPassenger = document.getElementById("addPassenger");
-let resetCounter = document.getElementById("resetCounter");
+let reset = document.getElementById("reset");
+let save = document.getElementById("save");
+let previousEntries = document.getElementById("entries");
 
 let count = 0;
 
 if (localStorage.getItem("counter")) {
   count = parseInt(localStorage.getItem("counter"));
   counter.innerHTML = count;
+}
+
+if (localStorage.getItem("previousEntries")) {
+  previousEntries.textContent = localStorage.getItem("previousEntries");
 }
 
 function updateCounter() {
@@ -19,7 +25,17 @@ addPassenger.addEventListener("click", () => {
   updateCounter();
 })
 
-resetCounter.addEventListener("click", () => {
+save.addEventListener("click", () => {
+  let countStr = count + " - ";
+  previousEntries.textContent += countStr;
+  localStorage.setItem("previousEntries", previousEntries.textContent);
+  count = 0;
+  updateCounter();
+})
+
+reset.addEventListener("click", () => {
+  previousEntries.textContent = "";
+  localStorage.setItem("previousEntries", previousEntries.textContent);
   count = 0;
   updateCounter();
 })
